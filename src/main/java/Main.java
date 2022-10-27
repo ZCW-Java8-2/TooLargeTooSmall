a6f83b4675e1589c3ac6aeddc3c214428f797b7a
Freddy <freddy@Zipcoders-MacBook-Pro-7.local>
2022-10-26 22:27:34 -0400

    submitting TooLargeTooSmall java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,50 @@
+import com.sun.source.util.DocTreePathScanner;
+
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+        //creating Scanner object with System.in parameter
+        Scanner scanner = new Scanner(System.in);
+        //creating range of random number between 0 & 20
+        int min = 0;
+        int max = 20;
+        int num = (int) (Math.random() * max) - min;
+        int guess = 0;
+        //random number generator test
+//        System.out.println(num);
+        //start the game
+        System.out.println("I am thinking of a number between 0 - 20. Guess the number. You have 4 tries");
+        guess = scanner.nextInt();
+        if (guess == num) {
+            System.out.println("Congratulations! The number is " + num);
+        } else {
+            //creating loop for number of tries
+            for (int i = 3; i >= 0; i--) {
+                //if guess is correct
+                if (guess == num) {
+                    System.out.println("Congratulations! The number is " + num);
+                    break;
+                }
+                //if you have 0 tries left
+                if (i == 0) {
+                    System.out.println("Game over :( The number is " + num);
+                    break;
+                }
+                //if guess is too low
+                if (guess > num) {
+                    System.out.println("Too large, try again. You have " + i + " tries left");
+                    guess = scanner.nextInt();
+                    //if guess is too high
+                } else {
+                    System.out.println("Too small, try again. You have " + i + " tries left");
+                    guess = scanner.nextInt();
+                }
+            }
+        }
     }
 }
