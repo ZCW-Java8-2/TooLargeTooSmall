874a647dc5dc1699960f3af6e92b7427d6d87bf3
Eryk <eryk@Zipcoders-MacBook-Pro-2.local>
2022-10-27 14:55:04 -0400

    fix consecutive same guesses

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,33 +5,36 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
-        int randomNumber = (int) (1 + (25 * Math.random())); //25
+        int max = 25;
+        int min = 0;
+        int randomNumber = (int) ((max+1) * Math.random()); //25
         boolean correctGuess = false;
         int guessCount = 0;
-        int lastGuess = -3;
-        System.out.println();
+        int lastGuess = min - 1 ;
+        System.out.println("random number is " + randomNumber);
         Scanner enterGuess = new Scanner(System.in);
         while(correctGuess == false){
-            System.out.println("Guess a number between 0 and 25. Please guess WHOLE NUMBERS ONLY");
+            System.out.println("Guess a number between 1 and 25. Please guess WHOLE NUMBERS ONLY");
             String guess = enterGuess.nextLine();
             int guessInt = Integer.parseInt(guess);
-            if(guessInt > 25){
+            if(lastGuess == guessInt && guessCount > 0) {
+                System.out.println("You guessed the same number twice in a row");
+            }else if(guessInt > max || guessInt < min) {
                 System.out.println("You guessed a number outside of the range.");
                 lastGuess = guessInt;
                 guessCount++;
-            }else if(guessInt < randomNumber){
-                System.out.println("guess higher next time.");
+            }else if(guessInt < randomNumber && guessInt > min - 1 ){
+                System.out.println("Guess higher next time.");
                 lastGuess = guessInt;
                 guessCount++;
-            }else if(guessInt > randomNumber && guessInt < 26){
-                System.out.println("guess lower next time.");
+            }else if(guessInt > randomNumber && guessInt < max + 1 ){
+                System.out.println("Guess lower next time.");
                 lastGuess = guessInt;
                 guessCount++;
             }else if(guessInt == randomNumber){
+                guessCount++;
                 System.out.println("You guessed right! " + "It took you " + guessCount + " guesses.");
                 correctGuess = true;
-            }else if(lastGuess == guessInt) {
-                System.out.println("you guessed the same number twice in a row");
             }
 
         }
